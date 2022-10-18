2c5b9934dca04ebeef737e5a679d4cc0804e8ce7
AndreiMalan <andreimalan10@gmail.com>
2022-10-18 18:10:52 +0300

    Add project files.

---
+++ Data/Malan_Andrei_Laborator2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Malan_Andrei_Laborator2.Models;
+
+namespace Malan_Andrei_Laborator2.Data
+{
+    public class Malan_Andrei_Laborator2Context : DbContext
+    {
+        public Malan_Andrei_Laborator2Context (DbContextOptions<Malan_Andrei_Laborator2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Malan_Andrei_Laborator2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Malan_Andrei_Laborator2.Models.Publisher> Publisher { get; set; }
+    }
+}
---
+++ Pages/Publishers/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Malan_Andrei_Laborator2.Data;
+using Malan_Andrei_Laborator2.Models;
+
+namespace Malan_Andrei_Laborator2.Pages.Publishers
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Malan_Andrei_Laborator2.Data.Malan_Andrei_Laborator2Context _context;
+
+        public DetailsModel(Malan_Andrei_Laborator2.Data.Malan_Andrei_Laborator2Context context)
+        {
+            _context = context;
+        }
+
+      public Publisher Publisher { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Publisher == null)
+            {
+                return NotFound();
+            }
+
+            var publisher = await _context.Publisher.FirstOrDefaultAsync(m => m.ID == id);
+            if (publisher == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Publisher = publisher;
+            }
+            return Page();
+        }
+    }
+}
